606b7560341164f9bd707b3d5b4ab5f87de06792
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-09-08 14:07:08 +0200

    commit discover menu cards

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 const linkDropdown = document.querySelectorAll(".link__dropdown");
 const dropdownWrapper = document.querySelectorAll('.dropdown-wrapper');
 
-console.log(linkDropdown);
-console.log(dropdownWrapper);
-
 linkDropdown.forEach((link, index) => {
     link.addEventListener("click", function() {
         dropdownWrapper[index].classList.toggle('active');
         linkDropdown[index].classList.toggle('active');
     })
 });
+
+const discoverBtn= document.querySelectorAll(".discover__button");
+const discoverCards = document.querySelectorAll(".discover__cards");
+
+discoverBtn.forEach(button => {
+    button.addEventListener("click", function() {
+        discoverBtn.forEach(button => {
+            button.classList.remove('active');
+        })
+
+        const cardFor = button.dataset.for;
+        discoverCards.forEach(card => {
+            card.classList.remove('active');
+        });
+
+        discoverCards.forEach( card => {
+           const cardTab = card.dataset.tab;
+           if (cardTab === cardFor) {
+               card.classList.add('active');
+           }
+        });
+
+        button.classList.add('active');
+    })
+})
